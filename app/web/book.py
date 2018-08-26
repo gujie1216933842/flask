bd870156458219d7cfd288bf75e2032012adaf10
gujie1216933842 <gujientsy@163.com>
2018-08-26 14:00:29 +0800

    fisher

--- app/web/book.py
+++ app/web/book.py
@@ -1,22 +1,32 @@
-from flask import jsonify
+from flask import jsonify, request
 from fisher import app
 from helper import is_isbn_or_key
 from yushu_book import YuShuBook
+from app.forms.book import SearchForm
 
 
 # 蓝图  buleprint
 
-@app.route('/book/search/<q>/<page>')
+@app.route('/book/search/')
 def search(q, page):
     '''
     :param q:
     :param page:
     :return:
     '''
-    isbn_or_key = is_isbn_or_key(q)
-    if isbn_or_key == 'isbn':
-        result = YuShuBook.search_isbn(q)
-    else:
-        result = YuShuBook.search_keyword(q)
 
-    return jsonify(result)
+    form = SearchForm(request.args)
+    if form.validate():
+        q = form.q.data.strip()
+        page = form.page.data
+
+        isbn_or_key = is_isbn_or_key(q)
+        if isbn_or_key == 'isbn':
+            result = YuShuBook.search_isbn(q)
+        else:
+            result = YuShuBook.search_keyword(q)
+
+        return jsonify(result)
+
+    else:
+        return jsonify({'msg': '参数失败'})

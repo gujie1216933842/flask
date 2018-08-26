5ed03d550bf51141bdba75117f1cd46a59ad9933
gujie1216933842 <gujientsy@163.com>
2018-08-26 12:52:13 +0800

    fisher

--- fisher.py
+++ fisher.py
@@ -1,22 +1,6 @@
-'''运行一个最简单的web程序'''
-from flask import Flask
+from fisher.app import create_app
 
-app = Flask(__name__)
-
-'''如果路由配置成  /hello/会重定向/hello'''
-
-
-@app.route('/hello')
-def hello():
-    return 'hello flask'
-
-
-def hello1():
-    return 'hello flask1'
-
+app = create_app()
 
 if __name__ == '__main__':
-    '''另一种路由注册的方式'''
-    app.add_url_rule('/hello1', view_func=hello1)
-    '''加入debug模式'''
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=app.config['DEBUG'], port=5000)

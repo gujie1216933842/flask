6c0d61db750c6efdb2c96f89e1fa133502b78286
gujie1216933842 <gujientsy@163.com>
2018-09-23 23:58:23 +0800

    fisher

--- app/models/user.py
+++ app/models/user.py
@@ -1,12 +1,16 @@
 from sqlalchemy import Column, Integer, String, Float, Boolean
 from flask_sqlalchemy import SQLAlchemy
 from app.models.base import Base
+from werkzeug.security import generate_password_hash
 
 
 class User(Base):
+    # __tablename__ = 'user1'  # 指定表名
+
     id = Column(Integer, primary_key=True)
     nickname = Column(String(24), nullable=False)
     phone_number = Column(String(18), unique=False)
+    _password = Column('password', String(128))
     email = Column(String(50), unique=True, nullable=False)
     confirmed = Column(Boolean, default=False)
     beans = Column(Float, default=0)
@@ -14,3 +18,11 @@ class User(Base):
     recieve_counter = Column(Integer, default=0)
     wx_open_id = Column(String(50))
     wx_name = Column(String(32))
+
+    @property
+    def password(self):
+        return self._password
+
+    @password.setter
+    def password(self, raw):
+        self._password = generate_password_hash(raw)

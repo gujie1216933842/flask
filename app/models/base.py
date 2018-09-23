441d242d6783823bce154a057a5e23b39411f767
gujie1216933842 <gujientsy@163.com>
2018-09-23 21:22:14 +0800

    fisher

--- app/models/base.py
+++ app/models/base.py
@@ -1,4 +1,10 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, SmallInteger
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+
+class Base(db.Model):
+    __abstract__ = True  # 作用: sqlalchemy不会去创建base表
+
+    status = Column(SmallInteger, default=1)

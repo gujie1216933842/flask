0bfe26ba3de5cd58770bcca75ebb7c9a565063dd
gujie1216933842 <gujientsy@163.com>
2018-09-24 11:21:39 +0800

    fisher

--- app/web/auth.py
+++ app/web/auth.py
@@ -2,13 +2,17 @@ from . import web
 from flask import render_template
 from app.forms.auth import RegisterForm
 from app.models.user import User
+from app.models.base import db
+
+
 @web.route('/register')
 def register(request):
     form = RegisterForm()  # 构建了一个验证form,当请求type是post的时候,才校验
-    if request.method=='POST' and form.validate():
+    if request.method == 'POST' and form.validate():
         user = User()
         user.set_attr(form.data)
-
+        db.session.add(user)
+        db.session.commit()
     return render_template('auth/register.html', form={'data:{}'})
 
 

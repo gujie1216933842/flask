9693568480d5e7013e268b1363938d42aa7002ad
gujie1216933842 <gujientsy@163.com>
2018-09-26 20:48:47 +0800

    fisher

--- app/web/auth.py
+++ app/web/auth.py
@@ -1,31 +1,38 @@
 from . import web
-from flask import render_template
+from flask import render_template, request
 from app.forms.auth import RegisterForm
 from app.models.user import User
 from app.models.base import db
 
 
-@web.route('/register')
-def register(request):
-    form = RegisterForm()  # 构建了一个验证form,当请求type是post的时候,才校验
-    if request.method == 'POST' and form.validate():
+@web.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm(request.form)  # 构建了一个验证form,当请求type是post的时候,才校验
+
+    print(request.method)
+    #
+    print(form.validate())
+
+    if request.method == 'POST' and not form.validate():
+        # print('hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh')
+
         user = User()
         user.set_attr(form.data)
         db.session.add(user)
         db.session.commit()
-    return render_template('auth/register.html', form={'data:{}'})
+    return render_template('auth/register.html', form={'data': {}})
 
 
-@web.route('/login', method=['GET', 'POST'])
-def login():
+@web.route('/login', methods=['GET', 'POST'])
+def login(request):
     pass
 
 
-@web.route('/reset/password', method=['GET', 'POST'])
-def forget_password_request():
+@web.route('/reset/password', methods=['GET', 'POST'])
+def forget_password_request(request):
     pass
 
 
-@web.route('/reset/password/<token>', method=['GET', 'POST'])
+@web.route('/reset/password/<token>', methods=['GET', 'POST'])
 def forget_password(token):
     pass
